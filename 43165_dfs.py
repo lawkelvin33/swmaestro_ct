46085f8b5f0939bddd490e873eceb34afe4d1a19
Jakyung <jakyung@Jakyungui-MacBookAir.local>
2023-02-22 16:52:15 +0900

    43162_dfs.py

--- 43165_dfs.py
+++ 43165_dfs.py
@@ -12,3 +12,14 @@ def dfs(numbers, index, target, total):
 
 ## 전형적인 dfs 문제 ##
 ## 200만 정도의 iteration
+
+##모범 답안##
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+    
+    ## solutions를 앞에서부터 하나씩 줄여나가면서 동시에 target의 수도 바꿔나가 계산하는 것이 핵심 ##
